23a6176bd09db487fa0b4d520b798cdaf1e58f1b
codeabuu <abdulngui718@gmail.com>
2023-04-11 21:40:12 +0000

    C program to print XML characters in reverse

---
+++ c-basic_1/rev.c
@@ -0,0 +1,10 @@
+#include <stdio.h>     
+int main()     
+{         
+	char a='X';         
+	char b='M';         
+	char c='L';          
+	printf("The test characters are %c,%c,%c\n",a,b,c);         
+	printf("The reverse of %c%c%c is %c%c%c\n",a,b,c,c,b,a);
+	return (0); 
+}
